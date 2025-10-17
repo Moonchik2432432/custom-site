3c30cb8f79639d99094326e1aa839afacd657c61
Ruslans Voronovs <rusvor2005@gmail.com>
2025-10-17 19:17:01 +0300

    Create create amats 0.0.1

--- resources/views/allAmats.blade.php
+++ resources/views/allAmats.blade.php
@@ -5,6 +5,10 @@
 <div class="container mt-4">
     <h1 class="mb-4">Amats tabula</h1>
 
+    <a href="/data/createAmats/{{$item->id}}">
+        <button class="btn btn-warning btn-sm">Rediģēt</button>
+    </a>
+
     @if($errors->any())
     @foreach ($errors->all() as $kluda)
         {{ $kluda }} <br><br>
@@ -51,21 +55,6 @@
             </tbody>
         </table>
     @endif
-
-    <form action="/data/newSubmitAmats" method="POST">
-        @csrf
-
-        <div class="container" style="max-width: 60%;">
-            <div class="mb-3">
-                <label for="nosaukums" class="form-label">Nosaukums</label>
-                <input type="text" class="form-control" id="nosaukums" name="nosaukums" placeholder="Nosaukums">
-            </div>
-
-            <button type="submit" class="btn btn-info">Saglabāt</button>
-        </div>
-    </form>
-    <br><br>
-    
 </div>
 
 @endsection
